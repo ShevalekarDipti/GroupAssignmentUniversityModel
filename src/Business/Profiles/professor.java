1572c0e442c7f0388d1abed0c298c2c66c44ec81
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2023-10-29 21:24:10 -0400

    git bash

---
+++ src/Business/Profiles/professor.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Profiles;
+
+/**
+ *
+ * @author dshev
+ */
+public class professor {
+    
+  
+    private String profLocation;
+    private String profUserName;
+    private String profPassword;
+    private String profResearchInterest;
+    private String profEmail;
+    private String ProfRole;
+
+    public String getProfRole() {
+        return ProfRole;
+    }
+
+    public void setProfRole(String ProfRole) {
+        this.ProfRole = ProfRole;
+    }
+   
+
+    public String getProfEmail() {
+        return profEmail;
+    }
+
+    public void setProfEmail(String profEmail) {
+        this.profEmail = profEmail;
+    }
+    
+    public String getProfResearchInterest() {
+        return profResearchInterest;
+    }
+
+    public void setProfResearchInterest(String profResearchInterest) {
+        this.profResearchInterest = profResearchInterest;
+    }
+
+   
+    public String getProfLocation() {
+        return profLocation;
+    }
+
+    public void setProfLocation(String profLocation) {
+        this.profLocation = profLocation;
+    }
+
+    public String getProfUserName() {
+        return profUserName;
+    }
+
+    public void setProfUserName(String profUserName) {
+        this.profUserName = profUserName;
+    }
+
+    public String getProfPassword() {
+        return profPassword;
+    }
+
+    public void setProfPassword(String profPassword) {
+        this.profPassword = profPassword;
+    }
+ 
+   
+}
---
+++ src/Business/Profiles/studentDirectory.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Profiles;
+
+import Business.Profiles.student;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dshev
+ */
+public class studentDirectory {
+    private ArrayList <student> studentList ;
+    
+     public studentDirectory() {
+        
+        this.studentList=new ArrayList<student>();
+        
+    }
+     
+    public ArrayList<student> getstudentList() {
+        return studentList;
+    }
+    
+    public student addStudent(){
+        student student = new student();
+        studentList.add(student);
+        return student;
+    }
+    
+      public void addStudent(student student){
+        studentList.add(student);
+        
+    }
+    
+    
+       public void deleteStudent(student student){
+        studentList.remove(student);
+        }
+       
+     public student searchStudent(String studID){
+        for(student student: studentList){
+            if(student.getStudID().equals(studID)){
+                return student;
+            }
+        }
+        return null; 
+    }  
+    
+    
+    
+}
